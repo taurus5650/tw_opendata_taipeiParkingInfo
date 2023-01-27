fafd79d37cb2d3ee5674af79c31219cef9329c06
Shyin <taurus_5650@hotmail.com>
2023-01-28 03:27:38 +0800

    Update main.py
    Refactor

--- main.py
+++ main.py
@@ -8,84 +8,57 @@ dataset : https://data.gov.tw/dataset/128435
 
 """
 
-
-import requests
 import json
+import textwrap
 
-url = "https://tcgbusfs.blob.core.windows.net/blobtcmsv/TCMSV_alldesc.json"
+import allure
+import requests
 
-resp = requests.get(url = url)
-respLoads = json.loads(resp.text)
+from utility.time import Time
 
-# print (json.dumps(respLoads, indent=2, ensure_ascii=False))
-
-def carMotorLessThan10 ():
-    print ("------  ------  ------  列出 car 和 motor 小於 5 的停車格數  ------  ------  ------")
-
-    # for result in respLoads :
-    #     carResult = respLoads["data"]["park"]
-    #     for result2 in carResult :
-    #         carFinal = result2["totalcar"]
-    #         if int(carFinal) <= 5 :
-    #             print (result2)
-
-    for result in respLoads["data"]["park"] :
-        car = result["totalcar"]
-        motor = result["totalmotor"]
-        if int(car) <= 5 and int(motor) <= 5 :
-            #print (result)
-            print (json.dumps(result, indent=2, ensure_ascii=False))
-
-def everyAreaCarAndMotor () :
-        print ("------  ------  ------  列出每個 area  的 car 和 motor 總停車格數  ------  ------  ------")
-        print ("e.g. 松山區、內湖區、大同區 etc.")
-        userInput = input(str("Input the area : "))
-
-        carSum = 0
-        motorSum = 0
-
-        for result in respLoads["data"]["park"]:
-            car = result["totalcar"]
-            motor = result["totalmotor"]
-            area = result["area"]
-            if userInput in str(area) :
-                carSum += int(car)
-            if userInput in str(area) :
-                motorSum += int(motor)
-            ## print area details
-            if userInput in str(area) :
-                print (result)
-
-        print (">>> TOTAL <<<")
-        print (f"Area : {str(userInput)} | Total Parking of Car {carSum}")
-        print (f"Area : {str(userInput)} | Total Parking of Motor {motorSum}")
-
-def carMotorTop5 () :
-    print ("------  ------  ------  列出 car 和 motor 相加總數，列出最高 5 名  ------  ------  ------")
-
-    carMotorTotalSum = []
-    N = 5
-
-    for result in respLoads["data"]["park"] :
-        id = result["id"]
-        name = result["name"]
-        car = result["totalcar"]
-        motor = result["totalmotor"]
-        carMotorTotalSum += [[str(id), str(name), int(car)+int(motor)]]
-
-    carMotorTotalSort = sorted(carMotorTotalSum, key=lambda x:x[2], reverse=True)[:N]
-
-    # print (carMotorTotalSum)
-    print (carMotorTotalSort)
-
-    ## pretty print
-    for finalPrint in carMotorTotalSort:
-        print ("id : ", finalPrint[0], end="")
-        print (" | name : ", finalPrint[1], end="")
-        print (" | total (car & motor) : ", finalPrint[2])
+from .configurations import GeneralConfig
 
 
-if __name__ == "__main__" :
-    carMotorLessThan10()
-    everyAreaCarAndMotor()
-    carMotorTop5 ()
+class BaseAPI:
+
+    def __init__(self, waiting_time=None):
+        self.waiting_time = waiting_time or GeneralConfig.DEFAULT_ACCEPTABLE_WAITING_TIME
+        self._session = requests.Session()
+
+    @allure.step("[{method}] {url}")
+    def _send_request(self, method: str, url: str, **kwargs):
+        acceptable_wating_time = kwargs.pop('waiting_time', None) or self.waiting_time
+        try:
+            response = self._session.request(method, url, **kwargs)
+            duration = response.elapsed.total_seconds()
+            self._debug_print(response=response)
+            assert duration <= acceptable_wating_time, (f"Response Time > {acceptable_wating_time}s, Cost: {duration}s")
+        except requests.exceptions.RequestException as e:
+            response = None
+            print(f"Request Error > url: [{method}] {url}, kwargs: {kwargs}, error: {str(e)}")
+        return response
+
+    def _debug_print(self, response: requests.Response):
+        req_body = response.request.body
+        if req_body:
+            req_body = json.loads(req_body)
+
+        information = {
+            'datetime': f"{Time.now():%Y/%m/%d %H:%M:%S}",
+            'request': f"[{response.request.method}] {response.request.url}",
+            'headers': json.dumps(dict(response.request.headers), indent=4, ensure_ascii=False),
+            'body': json.dumps(req_body, indent=4, ensure_ascii=False),
+            'elapsed': f"{response.elapsed.total_seconds()} s",
+            'status': response.status_code,
+            'response': json.dumps(response.json(), indent=4, ensure_ascii=False)
+        }
+
+        print(textwrap.dedent(
+            """
+            --------------------------------
+            🐞 debug prints
+            --------------------------------
+            {content}
+            --------------------------------
+            """
+        ).format(content=''.join([f"\n * {key}: {val}" for key, val in information.items()])))
